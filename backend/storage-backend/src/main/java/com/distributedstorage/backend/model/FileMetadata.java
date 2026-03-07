bfef19fff8ff25a575a2497f50478e6f4f5ddbc5
Akhtar Shah <akhtarshah8011@gmail.com>
2026-03-07 13:51:51 +0530

    Add FileMetadata entity model

---
+++ backend/storage-backend/src/main/java/com/distributedstorage/backend/model/FileMetadata.java
@@ -0,0 +1,40 @@
+package com.distributedstorage.backend.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class FileMetadata {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String fileName;
+    private String filePath;
+    private Long fileSize;
+    private LocalDateTime uploadedAt;
+
+    public FileMetadata(String fileName, String filePath, Long fileSize, LocalDateTime uploadedAt){
+        this.fileName = fileName;
+        this.filePath = filePath;
+        this.fileSize = fileSize;
+        this.uploadedAt = uploadedAt;
+    }
+
+    public Long getId(){
+        return id;
+    }
+    public String getFileName() {
+        return fileName;
+    }
+    public String getFilePath() {
+        return filePath;
+    }
+    public Long getFileSize() {
+        return fileSize;
+    }
+    public LocalDateTime getUploadedAt() {
+        return uploadedAt;
+    }
+    
+}
