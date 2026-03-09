6ab216db97090b19499c352bca372ca3f00544ad
Arzoo1901 <bansalarzoo1919@gmail.com>
2026-03-09 15:21:26 +0530

    Updated StorageService and TestStorage implementation

--- storage/storage-module/StorageService.java
+++ storage/storage-module/StorageService.java
@@ -1,38 +1,86 @@
+import java.io.*;
+
 public class StorageService {
 
-    private final String BASE_PATH = "storage/node1";
+    private static final String STORAGE_PATH = "storage/";
+    private static final int CHUNK_SIZE = 1024; // 1KB chunk size
 
-    public StorageService() {
-    java.io.File folder = new java.io.File(BASE_PATH);
+    public static void storeFile(String filePath) {
+        try {
+            File file = new File(filePath);
+            FileInputStream fis = new FileInputStream(file);
 
-        if (!folder.exists()) {
-            folder.mkdirs();
-            System.out.println("Storage folder created at: " + BASE_PATH);
-        } else {
-            System.out.println("Storage folder already exists.");
+            byte[] buffer = new byte[CHUNK_SIZE];
+            int bytesRead;
+            int chunkNumber = 1;
+
+            while ((bytesRead = fis.read(buffer)) != -1) {
+
+                String node = getNode(chunkNumber);
+                String chunkFileName = file.getName() + "_chunk_" + chunkNumber;
+
+                FileOutputStream fos = new FileOutputStream(
+                        STORAGE_PATH + node + "/" + chunkFileName);
+
+                fos.write(buffer, 0, bytesRead);
+                fos.close();
+
+                System.out.println("Stored " + chunkFileName + " in " + node);
+
+                chunkNumber++;
+            }
+
+            fis.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
-    public void saveFile(String sourcePath, String fileName) {
 
-        try {
-            java.io.FileInputStream input = new java.io.FileInputStream(sourcePath);
-            java.io.FileOutputStream output =
-                    new java.io.FileOutputStream(BASE_PATH + "/" + fileName);
+    public void downloadFile(String fileName) throws IOException {
+
+        String outputFile = "downloaded_" + fileName;
+
+        FileOutputStream fos = new FileOutputStream(outputFile);
+
+        for (int i = 1; i <= 3; i++) {
+
+            String nodePath = STORAGE_PATH + "node" + i + "/" + fileName + "_chunk_" + i;
+
+            File chunkFile = new File(nodePath);
+
+            if (!chunkFile.exists()) {
+                System.out.println("Chunk missing from node" + i);
+                continue;
+            }
+
+            FileInputStream fis = new FileInputStream(chunkFile);
 
             byte[] buffer = new byte[1024];
             int bytesRead;
 
-            while ((bytesRead = input.read(buffer)) != -1) {
-                output.write(buffer, 0, bytesRead);
+            while ((bytesRead = fis.read(buffer)) != -1) {
+                fos.write(buffer, 0, bytesRead);
             }
 
-            input.close();
-            output.close();
+            fis.close();
 
-            System.out.println("File saved successfully to " + BASE_PATH);
+            System.out.println("Retrieved chunk from node" + i);
+        }
 
-        } catch (Exception e) {
-            System.out.println("Error saving file: " + e.getMessage());
+        fos.close();
+
+        System.out.println("File reconstructed successfully as: " + outputFile);
+    }
+
+    private static String getNode(int chunkNumber) {
+
+        if (chunkNumber % 3 == 1) {
+            return "node1";
+        } else if (chunkNumber % 3 == 2) {
+            return "node2";
+        } else {
+            return "node3";
         }
     }
 }
